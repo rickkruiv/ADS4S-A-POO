8bf09520962c55049d995159b46faae2e8cd6929
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-08-27 20:25:52 -0300

    First Commit

---
+++ src/main/java/com/ads4s_a_poo/ads4s_a_poo/Ads4sAPooApplication.java
@@ -0,0 +1,13 @@
+package com.ads4s_a_poo.ads4s_a_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ads4sAPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ads4sAPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ads4s_a_poo/ads4s_a_poo/Ads4sAPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads4s_a_poo.ads4s_a_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ads4sAPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
