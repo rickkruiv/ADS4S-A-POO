6ccc7fe78cb64f90de9e919266deb736c77493a3
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-08-27 20:28:16 -0300

    Pessoa Model

---
+++ src/main/java/com/ads4s_a_poo/ads4s_a_poo/model/PessoaModel.java
@@ -0,0 +1,10 @@
+package com.ads4s_a_poo.ads4s_a_poo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity( name = "TBL_PESSOA" )
+@Table( name = "PESSOA" )
+public class PessoaModel {
+
+}
