93db56acf12c0a4bc5f135da0e93d40082ff7ec8
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-08-27 20:40:23 -0300

    Adicionado ID pessoa model

--- src/main/java/com/ads4s_a_poo/ads4s_a_poo/model/PessoaModel.java
+++ src/main/java/com/ads4s_a_poo/ads4s_a_poo/model/PessoaModel.java
@@ -1,10 +1,18 @@
 package com.ads4s_a_poo.ads4s_a_poo.model;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity( name = "TBL_PESSOA" )
 @Table( name = "PESSOA" )
 public class PessoaModel {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long Id;
 
 }
