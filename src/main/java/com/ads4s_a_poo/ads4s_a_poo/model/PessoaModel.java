8d24ede882a3363473e04fde22ff9efe8f599598
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-09-29 21:33:20 -0300

    Controller

--- src/main/java/com/ads4s_a_poo/ads4s_a_poo/model/PessoaModel.java
+++ src/main/java/com/ads4s_a_poo/ads4s_a_poo/model/PessoaModel.java
@@ -1,18 +1,73 @@
 package com.ads4s_a_poo.ads4s_a_poo.model;
 
-import jakarta.annotation.Generated;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
-@Entity( name = "TBL_PESSOA" )
-@Table( name = "PESSOA" )
+@Entity
+@Table(name = "PESSOA")
 public class PessoaModel {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
-    private Long Id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_PESSOA")
+    private Long id;
 
+    @Column(name = "NOME", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "EMAIL", nullable = false, unique = true, length = 120)
+    private String email;
+
+    @Column(name = "CPF", nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(name = "DATA_NASCIMENTO")
+    private LocalDate dataNascimento;
+
+    public PessoaModel(String nome, String email, String cpf, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
